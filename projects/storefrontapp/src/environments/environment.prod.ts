7eca1266ee1278bf811ef7cbee0b1ab5e8e50472
declantyson <declantyson@hotmail.co.uk>
2018-11-27 09:16:45 +0000

    [DT] Change env url

--- projects/storefrontapp/src/environments/environment.prod.ts
+++ projects/storefrontapp/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  occBaseUrl: 'https://electronics.spartacus.aws.e2y.co.uk/login.jsp'
+  occBaseUrl: 'https://ypay-automation-spartacus.aws.e2y.co.uk'
 };
